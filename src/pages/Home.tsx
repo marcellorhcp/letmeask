7e0cbc51fa6a17f9a2643c36f6ec75449930fd33
Marcelo Azevedo <marcellorhcp@gmail.com>
2021-07-28 00:40:34 -0300

    handleCreateRoom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,7 @@
+import { useHistory } from 'react-router-dom';
+
+import { auth, firebase } from '../services/firebase'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -7,6 +11,19 @@ import { Button } from '../components/Button';
 import '../styles/auth.scss';
 
 export function Home() {
+    const history = useHistory();
+
+    function handleCreateRoom() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result);
+        })
+
+
+        // history.push('/rooms/new');
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -17,7 +34,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="letmeask" />
-                    <button className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="logo do Google" />
                         Crie sua sala com o Google
                     </button>
